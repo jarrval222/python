1388e066312274cb4ff2304955319b07bc85b1fa
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-09-30 14:31:22 +0200

    Ejercicio 8 arreglado

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,6 +1,6 @@
-num1="Introduce un número: "
-num2="Introduce otro número: "
-div=num1/num2
+num1=float(input("Introduce un número: "))
+num2=float(input("Introduce otro número: "))
+div= num1/num2
 coc= num1 // num2
 res= num1 % num2
 print(f"La división es {div}, el cociente es {coc} y el resto es {res}")
