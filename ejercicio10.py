ff891e1c92d7e22d6889cc719a020003470fb05b
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-09-30 14:21:59 +0200

    Ejercicio10 terminado

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+numpayasos=int(input("Número de payasos: "))
+nummuñecas=int(input("Número de muñecas: "))
+pesopayaso=112
+pesomuñeca=75
+pesototal=(numpayasos*pesopayaso)+(nummuñecas*pesomuñeca)
+print(f"El peso total del paquete es de {pesototal} gramos")
