6f4d844f913c94885f7b2ddef6fa509b2038445a
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-09-30 13:55:00 +0200

    Ejercicio5 terminado

---
+++ ejercicio5.py
@@ -0,0 +1,4 @@
+hortrabaj=int(input("¿Cuántas horas has trabajado? "))
+costehora=float(input("¿Cuánto cobras por hora? "))
+sueldo=hortrabaj*costehora
+print(f"Tu sueldo es de " + str(sueldo) + " euros")
