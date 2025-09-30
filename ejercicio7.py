0e5bb11cc17be50174b67843533eadc73436f789
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-09-30 14:31:11 +0200

    Ejercicio 7 arreglado

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,4 +1,4 @@
-peso=("Introduce tu peso en kg: ")
-estatura=("Introduce tu estatura en metros: ")
-imc= round(peso/(estatura**2,2))
+peso=int(input("Introduce tu peso en kg: "))
+estatura=int(input("Introduce tu estatura en metros: "))
+imc= round(peso/(estatura ** 2),2)
 print(f"Tu índice de masa corporal es {imc}")
