2aa599cdabab6ab583c16d3943caf4cd310722c9
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-09-30 14:15:15 +0200

    Arreglo del ejercicio 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,3 +1,3 @@
 enteropos=int(input("Introduce un número entero positivo: "))
-res = sum(range(1, enteropos*(enteropos + 1)/2))
+res =  enteropos*(enteropos + 1)/2
 print(f"La suma de los números enteros positivos desde 1 hasta  {enteropos}   es   {res}")
