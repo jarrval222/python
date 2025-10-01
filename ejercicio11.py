0c7cd4c2718e4bdb3b22950cceca64fa1fc7ee57
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-10-01 09:52:29 +0200

    Ejercicio 11

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,8 +1,11 @@
 cantdin=float(input("Cantidad de dinero: "))
 interes= 0.04
 ahorro1= cantdin * 0.04 * 1
+cant1din= cantdin + ahorro1
 ahorro2= cantdin * 0.04 * 2
+cantdin2= cantdin + ahorro2
 ahorro3= cantdin * 0.04 * 3
-print (f"La cantidad en el primer año es de {cantdin + ahorro1}")
-print (f"La cantidad en el segundo año es de {cantdin + ahorro2}")
-print (f"La cantidad en el tercer año es de {cantdin + ahorro3}")
+cantdin3= cantdin + ahorro3
+print (f"La cantidad en el primer año es de {round(cant1din, 2)}")
+print (f"La cantidad en el segundo año es de {round(cantdin2, 2)}")
+print (f"La cantidad en el tercer año es de {round(cantdin3, 2)}")
