f9802bffe9ece1b7e23fe2ef0c53169bbfc1b20e
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-11-05 10:17:59 +0100

    ejercicio8 bucles

--- Ejpython4/ejercicio4.8.py
+++ Ejpython4/ejercicio4.8.py
@@ -1,5 +1,6 @@
 entero = int(input("Ingresa un número entero: "))
-for i in range(1, entero + 1):
-    for j in range(2 * i - 1, 0, -2):
-        print(j, end=" ")
+for i in range(1, entero + 1, 2):
     print()
+    for j in range(i, 0, -2):
+        print(j, end=" ")
+print()
