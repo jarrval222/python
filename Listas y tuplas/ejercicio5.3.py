0f2eb5b6f7f8ec15d173405271b3bcff98a1d9ab
jarrval222 <jarrval222@ieszaidinvergeles.org>
2025-12-02 14:34:27 +0100

    ejercicio3 listas

---
+++ Listas y tuplas/ejercicio5.3.py
@@ -0,0 +1,4 @@
+asignaturas = ["Mates", "Física", "Química", "Historia", "Lengua"]
+for i in asignaturas:
+    nota = int(input(f"Qué nota has sacado en {i}?:"))
+    print(f"En {i} has sacado {nota}")
