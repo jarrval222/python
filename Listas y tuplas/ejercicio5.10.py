c9737cedcd5ca5f814bdc5959d0d1a36667b2f44
jarrval222 <jarrval222@g.educaand.es>
2025-12-10 13:24:45 +0100

    ejercicio10 listas

---
+++ Listas y tuplas/ejercicio5.10.py
@@ -0,0 +1,6 @@
+precios = [50, 75, 46, 22, 80, 65, 8]
+precio_menor = min(precios)
+precio_mayor = max(precios)
+print(f"La lista de precios es: {precios}")
+print(f"El precio más bajo es: {precio_menor}")
+print(f"El precio más alto es: {precio_mayor}")
